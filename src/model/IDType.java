1d3faae7d9dabc8f6841a98224d2c8f32016a34d
Sinis2101 <sinisterra2101@gmail.com>
2021-04-08 11:24:30 -0500

    Person Class completed with attributes, constructor and getters. IDType works with enum.

---
+++ src/model/IDType.java
@@ -0,0 +1,7 @@
+package model;
+
+public enum IDType {
+
+    TI, CC, PP, CE;
+
+}
--- src/model/Person.java
+++ src/model/Person.java
@@ -2,6 +2,19 @@ package model;
 
 public class Person {
 
-    
+    private IDType idType;
+    private String id;
 
+    public Person(IDType idType, String id) {
+        this.idType = idType;
+        this.id = id;
+    }
+
+    public IDType getIdType() {
+        return idType;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
